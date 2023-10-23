71329e24c4175b3ef787b21f2804ca324cc04391
Vishaljain290502 <vishalj.aurasoft@gmail.com>
2023-10-23 11:00:01 +0530

    add

---
+++ script.js
@@ -0,0 +1,24 @@
+// const phone = document.querySelector("p");
+// const widthSize = window.matchMedia("(max-width: 880px)");
+
+// function home(e) {
+//     if (e.matches) {
+//       /* the viewport is 600 pixels wide or less */
+//       phone.img = "This is a narrow screen — 600px wide or less.";
+//       document.body.style.backgroundColor = "pink";
+//     } else {
+//       /* the viewport is more than 600 pixels wide */
+//       phone.textContent = "This is a wide screen — more than 600px wide.";
+//       document.body.style.backgroundColor = "aquamarine";
+//     }
+//   }
+
+// widthSize.addEventListener ('home')
+
+// const media =() => {
+//   let q = matchMedia("(max-width:700px)");
+//   if (q.matches) {
+//     document.querySelector("foote")
+//     HTMLBaseElement.
+//   }
+// }
